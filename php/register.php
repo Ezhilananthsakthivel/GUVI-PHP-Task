e05ee183def138026636529802ec866c2e477d35
Ezhilananthsakthivel <71553660+Ezhilananthsakthivel@users.noreply.github.com>
2023-02-07 13:04:14 +0530

    Update register.php

--- php/register.php
+++ php/register.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__ . './vendor/autoload.php';
+require './vendor/autoload.php';
 
 $con = mysqli_connect('localhost:3308','root','','guvi_task');
 $mcon = new MongoDB\Client("mongodb://localhost:27017");
@@ -12,7 +12,7 @@ $password = $_POST["password"];
 $user = mysqli_query($con,"select * from users where uname = '{$uname}'");
 if(mysqli_num_rows($user) > 0){
     echo "User Already Exists";
-    exit;
+    exit();
 }
 
 $sql = "insert into users (uname,email,password) values('{$uname}','{$email}','{$password}')";
