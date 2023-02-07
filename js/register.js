befb0f8ae92e1f1996e7d5433d82cc734bb04c56
Ezhilananthsakthivel <71553660+Ezhilananthsakthivel@users.noreply.github.com>
2023-02-07 13:14:18 +0530

    Update register.js

--- js/register.js
+++ js/register.js
@@ -1,12 +1,6 @@
 $(document).ready(function () {
     $('form').submit(function (e) {
         e.preventDefault()
-        // const data = {
-        //     uname: $('.uname').val(),
-        //     email: $('.email').val(),
-        //     password: $('.password').val(),
-        //     cpassword: $('.cpassword').val()
-        // }
 
         $.ajax({
             url: 'php/register.php',
@@ -16,12 +10,10 @@ $(document).ready(function () {
                 $('#submit').val('Loading...')
             },
             success: function (res) {
-                if (res) {
                     if (res != "User Already Exists") {
                         alert("Registration Successfully")
                         window.location = 'index.html'
                     }
-                }
                 else {
                     alert("User Already Exists")
                 }
