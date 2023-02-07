2878c05ce477986f61a3a250a805a1f248505ec0
Ezhilananthsakthivel <71553660+Ezhilananthsakthivel@users.noreply.github.com>
2023-02-07 16:38:17 +0530

    Update register.php

--- php/register.php
+++ php/register.php
@@ -1,12 +1,13 @@
 <?php
 
-require './vendor/autoload.php';
+require '../vendor/autoload.php';
 
 $con = mysqli_connect('localhost:3308','root','','guvi_task');
 $mcon = new MongoDB\Client("mongodb://localhost:27017");
 
-$uname = $_POST["uname"];
 $email = $_POST["email"];
+$pnumber = $_POST["pnumber"];
+$uname = $_POST["uname"];
 $password = $_POST["password"];
 
 $user = mysqli_query($con,"select * from users where uname = '$uname'");
@@ -15,16 +16,12 @@ if(mysqli_num_rows($user) > 0){
     echo "User Already Exists";
     exit();
 }
-else{
-    mysqli_query($con,"insert into users (uname,email,password) values ('$uname','$email','$password')");
+    
+    mysqli_query($con,"insert into users (uname,password) values ('$uname','$password')");
 
     $db = $mcon->guvitask->users;
-    $db->insertOne(['uname'=> $uname]);
-    
+    $db->insertOne(['uname'=> $uname,'email'=>$email, 'pnumber'=>$pnumber]);
     echo "Registration Successfully";
-
-}
-    
 
 
 ?>
